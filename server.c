53eef0305467efe59423be44f203e322b7f9c073
raul <raulgooo@outlook.com>
2026-01-04 04:50:56 -0600

    eliminated some slipped up code

--- server.c
+++ server.c
@@ -49,7 +49,7 @@ int main() {
         printf("Connection Correctly established with client.\n"); 
         bytes_read = read(fd_client, buffer_1,BUFFER_SIZE - 1);
         if (bytes_read > 0) {
-            buffer_1[bytes_read] = "\0";
+            buffer_1[bytes_read] = '\0';
             printf("%s", buffer_1);
         }
 
