bd40db0cc24770e2a491f14a41b230b90e6f3b73
raul <raulgooo@outlook.com>
2026-01-04 03:13:15 -0600

    Implemented basic tcp socket and binded it to a port

--- server.c
+++ server.c
@@ -7,3 +7,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
+#define PORT 8080;
+#define BUFFER_SIZE 1024;
+#define BACKLOG_SIZE 12;
+
+int main() {
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = PORT;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_socket < 0) {
+        perror("socket");
+        exit(1);
+    }
+    if (bind(tcp_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        exit(1);
+
+    }
+    printf("Succesful bind");
+}
